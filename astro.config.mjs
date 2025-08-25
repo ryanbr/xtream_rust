2b13bb376fc9b028d57bdf8377544699779626b0
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2025-08-25 11:41:59 +0200

    Update Astro configuration and styles for deployment
    - Set site URL and base path in `astro.config.mjs`.
    - Remove old CSS file `index.DVbX6mIk.css` and replace it with a new one `index.a0xD9TU3.css`.
    - Update `index.html` to reference the new CSS file.
    - Modify build script in `package.json` to output to `docs` directory.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dev.infinitel8p.com',
+  base: '/xtream/',
   vite: {
     plugins: [tailwindcss()]
   },
